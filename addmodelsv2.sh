06e9e6cb48044a534ae73cd8afa59c691241a1bb
a23sergiopn <a23sergiopn@iessanclemente.net>
2025-01-21 10:40:21 +0100

    No Hard links

--- addmodelsv2.sh
+++ addmodelsv2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#Version v1.0
+#Version v2.0
 TEMP_FILE=".temp" # TEMP FILE TO STORE THE MODELS NAMES
 
 if [ "$1" == "-h" ]; then # CALL THE HELP TO EXPLAIN HOW TO USE THE SCRIPT
@@ -14,11 +14,9 @@ if [ "$1" == "" ]; then
 else 
     if [ -d $1 ]; then #CHECK IF THE PROJECT EXIST
         cd $1
+        
         if [ -d "apps/$2" ]; then # CHECK IF THE APP EXISTS
             cd apps/$2
-            echo "@$(date)" >> .admin.py.old # ADD THE DATE TO THE BACKUP FILE
-            cat admin.py >> .admin.py.old # BACKUP THE ADMIN.PY FILE
-            echo "%" >> .admin.py.old 
             echo "Adding models from $2 to the admin view"
 
             echo 'from django.contrib import admin' > admin.py # CREATE OR CLEAN THE ADMIN.PY FILE with the import of admin
@@ -35,23 +33,20 @@ else
             echo "# Register your models here." >> admin.py # ADD A COMMENT
             echo "" >> admin.py # ADD A BLANK LINE
 
-            # CREATE THE ADMIN MODELS 
+            
 
             cat $TEMP_FILE | # GET THE MODELS NAMES 
             while read line; do # LOOP THE MODELS NAMES TO CREATE THE ADMIN MODEL
-                
+
+                echo "#MODEL $line" >> admin.py; # ADD A COMMENT
                 echo "class ${line}Admin(admin.ModelAdmin):" >> admin.py; # CREATE THE ADMIN MODEL
                 echo "    pass" >> admin.py; # Make the pass
                 echo "" >> admin.py; # ADD A BLANK LINE
-            done
-
-            # REGISTER THE MODELS TO THE ADMIN.PY
-
-            cat $TEMP_FILE | # GET THE MODELS NAMES 
-            while read line; do # LOOP THE MODELS NAMES TO ADD THE REGISTER TO THE ADMIN.PY
-                echo "$line registered" # PRINT THE MODEL NAME
                 echo "admin.site.register($line, ${line}Admin)" >> admin.py;
+                echo "#END MODEL $line" >> admin.py; # ADD A COMMENT
+                echo "" >> admin.py; # ADD A BLANK LINE
             done
+
         fi
     fi
 fi
@@ -61,6 +56,4 @@ fi
 
 
 
-#dahguidsfguisdfnbjusduvhuid
-
-#bjkfdsvhdfsuihudhgudsfhu
+#HAUIGHDFSIGDSFJGDFIOSNGUISDHGUISDNVNDSFIH
